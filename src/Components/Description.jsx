c26194f4db4dc1b7400e35f0d160f519b49233b4
Sachdarras <sachdarras@gmail.com>
2024-05-08 13:13:10 +0200

    desciption+photo

--- src/Components/Description.jsx
+++ src/Components/Description.jsx
@@ -1,13 +1,21 @@
+import profil from "../assets/images/profil.jpg";
 function Description() {
   return (
-    <div className="description-container">
-      <h2>Description</h2>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. A, ipsa laborum
-        provident dolores nemo sequi consequatur nobis doloribus autem similique
-        exercitationem sit velit? Unde, non? Repellat consequatur cupiditate
-        quam impedit.
-      </p>
+    <div className="content">
+      <div className="description-container">
+        <h2>A propos de</h2>
+        <h3>Alexandre-Sacha Darras</h3>
+        <h4>Developpeur web full-stack JavaScript </h4>
+        <p>
+          Après 10 ans passés dans le milieu du spectacle comme régisseur et
+          concepteur lumières. Il était temps de changer de vie, je me suis donc
+          orienté vers un métier qui reste créatif.C'est pour ça que mon regard,
+          c'est posé sur le développement web. Pour cela, je recherche une
+          alternance pour une durée de 15 mois, pour préparer un diplôme de
+          Concepteur Développeur d'Applications de niveau Bac+4.
+        </p>
+      </div>
+      <img src={profil} className="profil" />
     </div>
   );
 }
