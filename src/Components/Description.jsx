00136b39d83bff643cd860c4959aeb0dd8ac0bfc
Sachdarras <sachdarras@gmail.com>
2024-05-26 18:10:48 +0200

    corect orto

--- src/Components/Description.jsx
+++ src/Components/Description.jsx
@@ -9,9 +9,9 @@ function Description() {
         <p>
           Après 10 ans passés dans le milieu du spectacle comme régisseur et
           concepteur lumières, il était temps de changer de vie, je me suis donc
-          orienté vers un métier qui reste créatif. C'est pour ça que mon
-          regard, s'est posé sur le développement web. Pour cela, je recherche
-          une alternance pour une durée de 15 mois, pour préparer un diplôme de
+          orienté vers un métier qui reste créatif. C'est pour ça que mon regard
+          s'est posé sur le développement web. Pour cela, je recherche une
+          alternance pour une durée de 15 mois, pour préparer un diplôme de
           Concepteur Développeur d'Applications de niveau Bac+4.
         </p>
       </div>
